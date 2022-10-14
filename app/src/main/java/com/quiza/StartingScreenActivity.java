813c5af9695e1f16b65aed9587fdc673cde2a2d2
max83100 <gguvggvhj017@gmail.com>
2022-10-14 21:36:36 +0300

    14.10

--- app/src/main/java/com/quiza/StartingScreenActivity.java
+++ app/src/main/java/com/quiza/StartingScreenActivity.java
@@ -2,7 +2,10 @@ package com.quiza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class StartingScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+        Button start = findViewById(R.id.start_quiz);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        startActivity(new Intent(this,QuizActivity.class));
     }
 }
