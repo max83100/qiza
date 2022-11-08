4786f131a057e3ec937189e1ff9db322e91a179d
max83100 <gguvggvhj017@gmail.com>
2022-11-08 19:23:32 +0300

    8.11

--- app/src/main/java/com/quiza/StartingScreenActivity.kt
+++ app/src/main/java/com/quiza/StartingScreenActivity.kt
@@ -7,19 +7,25 @@ import com.quiza.R
 import android.content.Intent
 import android.view.View
 import android.widget.Button
+import androidx.core.app.TaskStackBuilder
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.quiza.QuizActivity
 
 class StartingScreenActivity : AppCompatActivity() {
-    private var textHighscore: TextView? = null
+    private lateinit var navController: NavController
     private var highscore = 0
     var newscore = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starting_screen)
-        val start = findViewById<Button>(R.id.start_quiz)
-        textHighscore = findViewById(R.id.text_score)
-        loadHighscore()
-        start.setOnClickListener { view: View? -> startQuiz() }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_component) as NavHostFragment
+        navController = navHostFragment.navController
+        //val start = findViewById<Button>(R.id.start_quiz)
+        //start.setOnClickListener { view: View? -> startQuiz() }
+        setupActionBarWithNavController(navController)
     }
 
     private fun startQuiz() {
@@ -27,12 +33,14 @@ class StartingScreenActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun loadHighscore() {
-        textHighscore!!.text = "Общий счёт: $newscore"
-    }
+
 
     private fun updateHighScore(newHighScore: Int) {
         highscore = newHighScore
-        textHighscore!!.text = "Общий счёт: $newscore"
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/quiza/ui/CategoryFragment.kt
+++ app/src/main/java/com/quiza/ui/CategoryFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+
 import com.quiza.R
 
 
 class CategoryFragment : Fragment() {
+    private lateinit var android_category: ImageButton
 
 
     override fun onCreateView(
@@ -16,8 +21,17 @@ class CategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        val view =  inflater.inflate(R.layout.fragment_category, container, false)
+        android_category = view.findViewById(R.id.android_category)
+        android_category.setOnClickListener {
+            findNavController().navigate(R.id.categoryFragment_to_quizFragment)
+        }
+
+        return view
     }
 
 
 }
+
+
+
