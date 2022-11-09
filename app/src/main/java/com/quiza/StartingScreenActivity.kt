4cc598f8e4f059def5e90756ec809e0198fabce4
max83100 <gguvggvhj017@gmail.com>
2022-11-09 18:30:00 +0300

    9.11

--- app/src/main/java/com/quiza/StartingScreenActivity.kt
+++ app/src/main/java/com/quiza/StartingScreenActivity.kt
@@ -15,30 +15,16 @@ import com.quiza.QuizActivity
 
 class StartingScreenActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private var highscore = 0
-    var newscore = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starting_screen)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_component) as NavHostFragment
         navController = navHostFragment.navController
-        //val start = findViewById<Button>(R.id.start_quiz)
-        //start.setOnClickListener { view: View? -> startQuiz() }
         setupActionBarWithNavController(navController)
     }
 
-    private fun startQuiz() {
-        val intent = Intent(this@StartingScreenActivity, QuizActivity::class.java)
-        startActivity(intent)
-    }
 
-
-
-    private fun updateHighScore(newHighScore: Int) {
-        highscore = newHighScore
-
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
